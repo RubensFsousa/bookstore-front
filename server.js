0cedfd551d2b66d2d4062447ab0816945fbec87e
RubensFsousa <xansxxl@gmail.com>
2022-10-10 21:49:58 -0300

    heroku routs fix

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
+const history = require('connect-history-api-fallback')
+const express = require('express')
+const path = require('path')
 
-var app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
 
-var port = process.env.PORT || 5000
+const app = express()
+const staticFileMiddleware = express.static(path.join(__dirname, 'dist'))
+
+app.use(history())
+app.use(staticFileMiddleware)
+
+app.get('/', function (req, res) {
+    res.render(path.join(__dirname + '/index.html'))
+})
+
+var port = process.env.PORT || 8080
 app.listen(port)
 console.log('server started ' + port)
--- src/service/config.js
+++ src/service/config.js
@@ -1,5 +1,5 @@
-import axios from "axios"
+import axios from 'axios';
 
 export const http = axios.create({
-    baseURL: 'http://localhost:8080/api/'
-})
+    baseURL: 'https://api-spring-bookstore.herokuapp.com/'
+});
